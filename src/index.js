42818299565ed53f94f391b477a85b658c773991
Muhammad Wasif Khan <wasifp9@gmail.com>
2025-10-16 17:30:33 +0500

    Worked on Backend of Hotel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv"
 import { app } from "./app.js"
+import connectDB from "./db/index.js"
 
 dotenv.config({
     path:'./.env'
 })
 
+
+connectDB()
 
 app.listen(process.env.PORT || 8000,()=>{
     console.log(`Server is Running at port : ${process.env.PORT}`)
